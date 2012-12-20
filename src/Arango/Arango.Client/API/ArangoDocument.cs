d1800041d6423a8217e9ff6daf0c58e22507dd7a
Tomas Bosak <bosak.tomas@gmail.com>
2012-12-20 22:38:55 +0100

    ArangoDocument object stores json data in Json property which is dynamic ExpandoObject and it's fields can be checked with Has method.

--- src/Arango/Arango.Client/API/ArangoDocument.cs
+++ src/Arango/Arango.Client/API/ArangoDocument.cs
@@ -1,11 +1,53 @@
-﻿
+﻿//using System;
+using System.Collections.Generic;
+using System.Dynamic;
+
 namespace Arango.Client
 {
     public class ArangoDocument
     {
         public string ID { get; set; }
         public string Revision { get; set; }
-
         public string Data { get; set; }
+        public dynamic Json { get; set; }
+
+        public ArangoDocument()
+        {
+            Json = new ExpandoObject();
+        }
+
+        public bool Has(string fieldName)
+        {
+            if (fieldName.Contains("."))
+            {
+                var fields = fieldName.Split('.');
+                var iteration = 1;
+                var json = (IDictionary<string, object>)Json;
+
+                foreach (var field in fields)
+                {
+                    if (json.ContainsKey(field))
+                    {
+                        if (iteration == fields.Length)
+                        {
+                            return true;
+                        }
+
+                        json = (IDictionary<string, object>)json[field];
+                        iteration++;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+            }
+            else
+            {
+                return ((IDictionary<string, object>)Json).ContainsKey(fieldName);
+            }
+
+            return false;
+        }
     }
 }
--- src/Arango/Arango.Console/Program.cs
+++ src/Arango/Arango.Console/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.IO;
 using Arango.Client;
+using System.Dynamic;
 
 namespace Arango.Console
 {
@@ -25,6 +26,17 @@ namespace Arango.Console
             ArangoDocument document = database.GetDocument("10843274/12481674", "x12481674");
 
             System.Console.WriteLine("ID: {0}, Rev: {1}, Data: {2}", document.ID, document.Revision, document.Data);
+
+            ArangoDocument doc = new ArangoDocument();
+            doc.Json.foo = "abc";
+            doc.Json.bar = new ExpandoObject();
+            doc.Json.bar.baz = 123;
+
+            System.Console.WriteLine("foo: {0} {1}", doc.Has("foo"), doc.Json.foo);
+            System.Console.WriteLine("bar: {0} {1}", doc.Has("bar"), doc.Json.bar);
+            System.Console.WriteLine("bar.baz: {0} {1}", doc.Has("bar.baz"), doc.Json.bar.baz);
+            System.Console.WriteLine("non: {0}", doc.Has("non"));
+            System.Console.WriteLine("non.exist: {0}", doc.Has("non.exist"));
 
             System.Console.ReadLine();
         }
