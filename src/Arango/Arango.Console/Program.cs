fa4597f96a3d30afcfc6cb18ddc5756c064486be
Tomas Bosak <bosak.tomas@gmail.com>
2012-12-22 17:20:21 +0100

    Apply Json and Object changes to console test.

--- src/Arango/Arango.Console/Program.cs
+++ src/Arango/Arango.Console/Program.cs
@@ -25,12 +25,12 @@ namespace Arango.Console
             ArangoDatabase database = new ArangoDatabase(alias);
 
             ArangoDocument document = database.GetDocument("10843274/12481674", "x12481674");
-            System.Console.WriteLine("Handle: {0}, Rev: {1}, Data: {2}", document.Handle, document.Revision, document.Data);
+            System.Console.WriteLine("Handle: {0}, Rev: {1}, Json: {2}", document.Handle, document.Revision, document.Json);
 
             ArangoCollection collection = database.GetCollection(10843274);
-            System.Console.WriteLine("ID: {0}, Name: {1}", collection.ID, collection.Name);
+            System.Console.WriteLine("ID: {0}, Name: {1}, Status: {2}, Type: {3}", collection.ID, collection.Name, collection.Status, collection.Type);
 
-            ArangoDocument doc = new ArangoDocument();
+            /*ArangoDocument doc = new ArangoDocument();
             doc.Json.foo = "abc";
             doc.Json.bar = new ExpandoObject();
             doc.Json.bar.baz = 123;
@@ -39,7 +39,7 @@ namespace Arango.Console
             System.Console.WriteLine("bar: {0} {1}", doc.Has("bar"), doc.Json.bar);
             System.Console.WriteLine("bar.baz: {0} {1}", doc.Has("bar.baz"), doc.Json.bar.baz);
             System.Console.WriteLine("non: {0}", doc.Has("non"));
-            System.Console.WriteLine("non.exist: {0}", doc.Has("non.exist"));
+            System.Console.WriteLine("non.exist: {0}", doc.Has("non.exist"));*/
 
             System.Console.ReadLine();
         }
