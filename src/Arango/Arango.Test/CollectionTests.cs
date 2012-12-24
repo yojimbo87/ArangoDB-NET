3f07471cff190164436626bf719aab3173936d3b
Tomas Bosak <bosak.tomas@gmail.com>
2012-12-24 22:39:17 +0100

    Make tests for delete collection methods.

--- src/Arango/Arango.Test/CollectionTests.cs
+++ src/Arango/Arango.Test/CollectionTests.cs
@@ -1,14 +1,72 @@
 ﻿using System;
+using System.IO;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Arango.Client;
 
 namespace Arango.Test
 {
     [TestClass]
     public class CollectionTests
     {
-        [TestMethod]
-        public void TestMethod1()
+        private ArangoDatabase _database;
+
+        public CollectionTests()
         {
+            string alias = "test";
+            string[] connectionString = File.ReadAllText(@"..\..\..\..\..\ConnectionString.txt").Split(';');
+
+            ArangoNode node = new ArangoNode(
+                connectionString[0],
+                int.Parse(connectionString[1]),
+                connectionString[2],
+                connectionString[3],
+                alias
+            );
+            ArangoClient.Nodes.Add(node);
+
+            _database = new ArangoDatabase(alias);
+        }
+
+        [TestMethod]
+        public void CreateCollectionAndDeleteItByID()
+        {
+            ArangoCollection testCollection = new ArangoCollection();
+            testCollection.Name = "tempUnitTestCollection001xyz";
+            testCollection.Type = ArangoCollectionType.Document;
+            testCollection.WaitForSync = false;
+            testCollection.JournalSize = 1024 * 1024; // 1 MB
+
+            ArangoCollection newCollection = _database.CreateCollection(testCollection.Name, testCollection.Type, testCollection.WaitForSync, testCollection.JournalSize);
+
+            Assert.AreEqual(testCollection.Name, newCollection.Name);
+            Assert.AreEqual(testCollection.Type, newCollection.Type);
+            Assert.AreEqual(testCollection.WaitForSync, newCollection.WaitForSync);
+            Assert.AreEqual(testCollection.JournalSize, newCollection.JournalSize);
+
+            bool isDeleted = _database.DeleteCollection(newCollection.ID);
+
+            Assert.IsTrue(isDeleted);
+        }
+
+        [TestMethod]
+        public void CreateCollectionAndDeleteItByName()
+        {
+            ArangoCollection testCollection = new ArangoCollection();
+            testCollection.Name = "tempUnitTestCollection002xyz";
+            testCollection.Type = ArangoCollectionType.Document;
+            testCollection.WaitForSync = false;
+            testCollection.JournalSize = 1024 * 1024; // 1 MB
+
+            ArangoCollection newCollection = _database.CreateCollection(testCollection.Name, testCollection.Type, testCollection.WaitForSync, testCollection.JournalSize);
+
+            Assert.AreEqual(testCollection.Name, newCollection.Name);
+            Assert.AreEqual(testCollection.Type, newCollection.Type);
+            Assert.AreEqual(testCollection.WaitForSync, newCollection.WaitForSync);
+            Assert.AreEqual(testCollection.JournalSize, newCollection.JournalSize);
+
+            bool isDeleted = _database.DeleteCollection(newCollection.Name);
+
+            Assert.IsTrue(isDeleted);
         }
     }
 }
