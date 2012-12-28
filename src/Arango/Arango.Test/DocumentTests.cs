42ecf0cdb62201ec4ad275337f790ca09e212ebd
Tomas Bosak <bosak.tomas@gmail.com>
2012-12-28 16:01:54 +0100

    Prepare tests for documents API.

---
+++ src/Arango/Arango.Test/DocumentTests.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Arango.Client;
+
+namespace Arango.Test
+{
+    [TestClass]
+    public class DocumentTests
+    {
+        private ArangoDatabase _database;
+
+        public DocumentTests()
+        {
+            string alias = "test";
+            string[] connectionString = File.ReadAllText(@"..\..\..\..\..\ConnectionString.txt").Split(';');
+
+            ArangoNode node = new ArangoNode(
+                connectionString[0],
+                int.Parse(connectionString[1]),
+                connectionString[2],
+                connectionString[3],
+                alias
+            );
+            ArangoClient.Nodes.Add(node);
+
+            _database = new ArangoDatabase(alias);
+        }
+
+        #region Get
+
+        [TestMethod]
+        public void GetDocumentByHandle()
+        {
+        }
+
+        [TestMethod]
+        public void GetDocumentByHandleAndRevision()
+        {
+        }
+
+        #endregion
+    }
+}
