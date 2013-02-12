15c614ba6bc226a99d0a435c822d776b1e4c347b
Tomas Bosak <bosak.tomas@gmail.com>
2013-02-12 23:16:28 +0100

    Add test for basic cursor query.

--- src/Arango/Arango.Console/Program.cs
+++ src/Arango/Arango.Console/Program.cs
@@ -45,9 +45,8 @@ namespace Arango.Console
             System.Console.WriteLine("non.exist: {0}", doc.Has("non.exist"));*/
 
             //TestGet();
-            //TestQuery();
-            //TestText();
-            TestDictionary();
+            TestQuery();
+            //TestLoad();
 
             System.Console.ReadLine();
         }
@@ -62,79 +61,65 @@ namespace Arango.Console
         static void TestQuery()
         {
             Dictionary<string, string> values = new Dictionary<string, string>();
-            values.Add("@title", "Test kiosk number 5");
+            values.Add("title", "Test kiosk number 5");
             List<ArangoDocument> documents = _database.Query("FOR k IN Kiosks FILTER k.Title == @title RETURN k", false, 2, values);
+
+            //List<ArangoDocument> documents = _database.Query("FOR k IN Kiosks RETURN k");
 
             documents.ForEach(d => System.Console.WriteLine(d.ID));
         }
 
-        static void TestText()
+        static void TestLoad()
         {
-            /*JsonObject json = "{\"foo\":{\"bar\":[{\"baz\":123},{\"baz\":456}]}}".ToJson();
+            int repetitions = 20;
+            long total = 0;
 
-            List<Json> bar = json.Get<JsonObject>("foo").Get<List<Json>>("bar");
-            System.Console.WriteLine(bar[0].Get<int>("baz"));
-            
-            JsonObject baz = new JsonObject();
-            baz.Add("baz", );
-            bar.Add(
-            
-            System.Console.WriteLine(json.Stringify());*/
+            for (int i = 0; i < repetitions; i++)
+            {
+                long tps = Do();
+                total += tps;
+
+                System.Console.WriteLine("TPS: " + tps);
+            }
+
+            System.Console.WriteLine("Average: " + total / repetitions);
         }
 
-        static void TestDictionary()
+        static long Do()
         {
-            /*Foo json = new Foo();
-            json.Add("stringKey", "string value");
-            json.Add("numericKey", 12321);
+            DateTime start = DateTime.Now;
+            bool running = true;
+            long tps = 0;
 
-            Foo embeddedJson = new Foo();
-            embeddedJson.Add("stringKey", "string value");
-            embeddedJson.Add("numericKey", 12321);
+            do
+            {
+                List<ArangoDocument> documents = _database.Query("FOR k IN Kiosks RETURN k");
+                tps++;
 
-            json.Add("innerObjectKey", embeddedJson);
+                TimeSpan dif = DateTime.Now - start;
 
-            List<Foo> arrayKey = new List<Foo>();
-            arrayKey.Add(embeddedJson);
-            arrayKey.Add(embeddedJson);
+                if (dif.TotalMilliseconds > 1000)
+                {
+                    running = false;
+                }
+            }
+            while (running);
 
-            json.Add("arrayKey", arrayKey);
+            return tps;
 
-            Foo embeddedJson2 = new Foo();
-            embeddedJson2.Add("stringKey", "string value");
-            embeddedJson2.Add("numericKey", 12321);
-            embeddedJson2.Add("arrayKey", arrayKey);
+            /*for (int i = 0; i < 1000; i++)
+            {
+                List<RexsterVertex> vertices = graph.Gremlin<RexsterVertex>("vx", new string[] { "vertices" });
+            }
 
-            json.Add("embeddedKey", embeddedJson2);
+            //Parallel.For(0, 1000, i =>
+            //{
+            //    List<RexsterVertex> vertices = graph.Gremlin<RexsterVertex>("vx", new string[] { "vertices" });
+            //});
 
-            System.Console.WriteLine(json.ToJson());*/
+            TimeSpan dif = DateTime.Now - start;
 
-            string s = "{\"stringKey\":\"string value\",\"numericKey\":12321,\"arrayEmptyKey\":[],\"arrayKey\":[1,2,3],\"arrayEmbeddedKey\":[{\"foo\":123},{\"foo\":456}],\"embeddedKey\":{\"stringKey\":\"string value\",\"numeric\":321,\"array\":[3,2,1]}}";
-            string s2 = "{\"array\":[\"whoa\",123]}";
-            string s3 = "[\"a\",\"b\"]";
-            //Foo json = s2.FromJson<Foo>();
-
-            Json foo = new Json();
-
-            foo.Load(s);
-
-            foo.PrintDump();
-            System.Console.WriteLine(foo.Has("embeddedKey.numeric"));
-            System.Console.WriteLine(foo.Has("bbb.aaa"));
-            System.Console.WriteLine(foo.Get("embeddedKey.stringKey"));
-            System.Console.WriteLine(foo.Get("aaa"));
-            System.Console.WriteLine(foo.Get<int>("embeddedKey.numeric"));
-
-            foo.Set("embeddedKey.numeric", 456);
-            System.Console.WriteLine(foo.Get<int>("embeddedKey.numeric"));
-
-            System.Console.WriteLine(foo.Get<int>("aaa"));
-            System.Console.WriteLine(foo.Stringify());
+            System.Console.WriteLine("Time: " + dif.TotalSeconds);*/
         }
-    }
-
-    public class Foo : Dictionary<string, object>
-    {
-        //public int bar { get; set; }
     }
 }
---
+++ src/Arango/Arango.Test/CursorTests.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Arango.Client;
+
+namespace Arango.Test
+{
+    [TestClass]
+    public class CursorTests : IDisposable
+    {
+        private ArangoDatabase _database;
+        private ArangoCollection _collection;
+
+        public CursorTests()
+        {
+            string alias = "test";
+            string testCollectionName = "tempUnitTestCollectionForDocumentTests001xyz";
+            string[] connectionString = File.ReadAllText(@"..\..\..\..\..\ConnectionString.txt").Split(';');
+
+            // create aliased arango node
+            ArangoNode node = new ArangoNode(
+                connectionString[0],
+                int.Parse(connectionString[1]),
+                connectionString[2],
+                connectionString[3],
+                alias
+            );
+            ArangoClient.Nodes.Add(node);
+
+            _database = new ArangoDatabase(alias);
+
+            // delete test collection if it already exist
+            /*ArangoCollection col = _database.GetCollection(testCollectionName);
+
+            if ((col != null) && !string.IsNullOrEmpty(col.Name))
+            {
+                _database.DeleteCollection(testCollectionName);
+            }*/
+
+            // create test collection
+            ArangoCollection testCollection = new ArangoCollection();
+            testCollection.Name = testCollectionName;
+            testCollection.Type = ArangoCollectionType.Document;
+            testCollection.WaitForSync = false;
+            testCollection.JournalSize = 1024 * 1024; // 1 MB
+
+            _collection = _database.CreateCollection(testCollection.Name, testCollection.Type, testCollection.WaitForSync, testCollection.JournalSize);
+
+            Json doc = new Json();
+            doc.Set("Name", "Johny");
+            doc.Set("Surname", "Bravo");
+            doc.Set("Age", 18);
+            _database.CreateDocument(_collection.ID, doc, false);
+
+            doc = new Json();
+            doc.Set("Name", "Frankie");
+            doc.Set("Surname", "Hoover");
+            doc.Set("Age", 25);
+            _database.CreateDocument(_collection.ID, doc, false);
+
+            doc = new Json();
+            doc.Set("Name", "Maria");
+            doc.Set("Surname", "Cool");
+            doc.Set("Age", 32);
+            _database.CreateDocument(_collection.ID, doc, false);
+
+            doc = new Json();
+            doc.Set("Name", "Mario");
+            doc.Set("Surname", "Bro");
+            doc.Set("Age", 43);
+            _database.CreateDocument(_collection.ID, doc, false);
+        }
+
+        [TestMethod]
+        public void BacisCursorQuery()
+        {
+            List<ArangoDocument> documents = _database.Query("FOR u IN " + _collection.Name + " RETURN u");
+
+            Assert.AreEqual(documents.Count, 4);
+        }
+
+        public void Dispose()
+        {
+            _database.DeleteCollection(_collection.ID);
+        }
+    }
+}
