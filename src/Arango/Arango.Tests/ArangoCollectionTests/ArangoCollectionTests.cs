98280c518cb5e1c78b7e3857e801a0b94f24dcbf
Tomas Bosak <bosak.tomas@gmail.com>
2013-07-04 16:27:18 +0200

    Move tests to separate folders and create tests for collections.

---
+++ src/Arango/Arango.Tests/ArangoCollectionTests/ArangoCollectionTests.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+using Arango.Client;
+
+namespace Arango.Tests.ArangoCollectionTests
+{
+    [TestFixture()]
+    public class ArangoCollectionTests
+    {
+        [Test()]
+        public void Should_create_and_delete_collection()
+        {
+            ArangoDatabase db = Database.GetTestDatabase();
+            
+            // set collection data
+            ArangoCollection collection = new ArangoCollection();
+            collection.Name = Database.TestCollectionName;
+            collection.Type = ArangoCollectionType.Edge;
+            collection.WaitForSync = true;
+            
+            // create collection in database
+            db.Collection.Create(collection);
+            
+            // check collection data retrieved from server
+            Assert.AreEqual(false, string.IsNullOrEmpty(collection.Id));
+            Assert.AreEqual(Database.TestCollectionName, collection.Name);
+            Assert.AreEqual(ArangoCollectionType.Edge, collection.Type);
+            Assert.AreEqual(ArangoCollectionStatus.Loaded, collection.Status);
+            Assert.AreEqual(true, collection.WaitForSync);
+            Assert.AreEqual(false, collection.IsSystem);
+            Assert.AreEqual(false, collection.IsVolatile);
+            
+            // delete collection from database
+            bool isDeleted = db.Collection.Delete(collection.Name);
+            
+            // check if the collection was deleted from database
+            Assert.AreEqual(true, isDeleted);
+        }
+    }
+}
---
+++ src/Arango/Arango.Tests/Database.cs
@@ -0,0 +1,42 @@
+﻿using Arango.Client;
+
+namespace Arango.Tests
+{
+    public static class Database
+    {
+        public static string Hostname { get; set; }
+        public static int Port { get; set; }
+        public static bool IsSecured { get; set; }
+        public static string UserName { get; set; }
+        public static string Password { get; set; }
+        public static string Alias { get; set; }
+        
+        public static string TestCollectionName { get; set; }
+        
+        static Database()
+        {
+            Hostname = "localhost";
+            Port = 8529;
+            IsSecured = false;
+            UserName = "";
+            Password = "";
+            Alias = "test";
+            
+            TestCollectionName = "testCollection001xyzLatif";
+            
+            ArangoClient.AddDatabase(
+                Hostname,
+                Port,
+                IsSecured,
+                UserName,
+                Password,
+                Alias
+            );
+        }
+        
+        public static ArangoDatabase GetTestDatabase()
+        {
+            return new ArangoDatabase(Alias);
+        }
+    }
+}
