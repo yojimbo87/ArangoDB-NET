5793d6c3e7c0be2257fa8e7738a38d7d53b1331f
Tomas Bosak <bosak.tomas@gmail.com>
2013-07-05 12:10:23 +0200

    Implement create and delete operations for documents.

--- src/Arango/Arango.Client/API/Documents/ArangoDocumentOperation.cs
+++ src/Arango/Arango.Client/API/Documents/ArangoDocumentOperation.cs
@@ -15,6 +15,16 @@ namespace Arango.Client
         {
             return _documentOperation.Get(id);
         }
+        
+        public void Create(string collection, ArangoDocument arangoDocument, bool waitForSync = false, bool createCollection = false)
+        {
+            _documentOperation.Post(collection, arangoDocument, waitForSync, createCollection);
+        }
+        
+        public bool Delete(string id)
+        {
+            return _documentOperation.Delete(id);
+        }
     }
 }
 
--- src/Arango/Arango.Client/Protocol/Operations/DocumentOperation.cs
+++ src/Arango/Arango.Client/Protocol/Operations/DocumentOperation.cs
@@ -18,8 +18,8 @@ namespace Arango.Client.Protocol
         internal ArangoDocument Get(string id)
         {
             var request = new Request();
-            request.RelativeUri = string.Join("/", _apiUri, id);
             request.Method = HttpMethod.Get;
+            request.RelativeUri = string.Join("/", _apiUri, id);
 
             return Get(request);
         }
@@ -44,6 +44,73 @@ namespace Arango.Client.Protocol
             return document;
         }
 
+        #endregion
+        
+        #region POST
+        
+        internal void Post(string collection, ArangoDocument arangoDocument, bool waitForSync, bool createCollection)
+        {
+            var request = new Request();
+            request.Method = HttpMethod.Post;
+            request.RelativeUri = _apiUri;
+            request.Body = arangoDocument.Serialize();
+            
+            // set collection name where the document will be created
+            request.QueryString.Add("collection", collection);
+            
+            // (optional)
+            if (createCollection)
+            {
+                request.QueryString.Add("createCollection", "true");
+            }
+            
+            // (optional)
+            if (waitForSync)
+            {
+                request.QueryString.Add("waitForSync", "true");
+            }
+            
+            var response = _connection.Process(request);
+            
+            switch (response.StatusCode)
+            {
+                case HttpStatusCode.Created:
+                case HttpStatusCode.Accepted:
+                    arangoDocument.Id = response.Document.GetField<string>("_id");
+                    arangoDocument.Key = response.Document.GetField<string>("_key");
+                    arangoDocument.Revision = response.Document.GetField<string>("_rev");
+                    break;
+                default:
+                    break;
+            }
+        }
+        
+        #endregion
+        
+        #region DELETE
+        
+        internal bool Delete(string id)
+        {
+            var request = new Request();
+            request.RelativeUri = string.Join("/", _apiUri, id);
+            request.Method = HttpMethod.Delete;
+            
+            var response = _connection.Process(request);
+            var isRemoved = false;
+
+            switch (response.StatusCode)
+            {
+                case HttpStatusCode.OK:
+                case HttpStatusCode.Accepted:
+                    isRemoved = true;
+                    break;
+                default:
+                    break;
+            }
+            
+            return isRemoved;
+        }
+        
         #endregion
     }
 }
