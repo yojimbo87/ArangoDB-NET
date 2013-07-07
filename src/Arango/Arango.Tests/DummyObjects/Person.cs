f9c73013ced473bc4cd3ddb53f7617e6663d52f5
Tomas Bosak <bosak.tomas@gmail.com>
2013-07-07 19:30:12 +0200

    Create dummy test object.

---
+++ src/Arango/Arango.Tests/DummyObjects/Person.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using Arango.Client;
+
+namespace Arango.Tests
+{
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        
+        [ArangoProperty(Serializable = false)]
+        public string ShouldNotBeSerialized { get; set; }
+        
+        [ArangoProperty(Alias = "aliasedField")]
+        public string Aliased { get; set; }
+        
+        public List<string> Interests { get; set; }
+        public List<Person> Followers { get; set; }
+        
+        public Person()
+        {
+            Interests = new List<string>();
+            Followers = new List<Person>();
+        }
+    }
+}
