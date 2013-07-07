53ab1cf4f471c3a8ad8eb60719fe8c5ae20b18a4
Tomas Bosak <bosak.tomas@gmail.com>
2013-07-07 19:29:39 +0200

    Implement custom property to allow object properties being flagged with attributes.

---
+++ src/Arango/Arango.Client/API/Documents/ArangoProperty.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Arango.Client
+{
+    [AttributeUsage(AttributeTargets.Property)]
+    public class ArangoProperty : Attribute
+    {
+        public string Alias { get; set; }
+        public bool Serializable { get; set; }
+        
+        public ArangoProperty()
+        {
+            Serializable = true;
+        }
+    }
+}
