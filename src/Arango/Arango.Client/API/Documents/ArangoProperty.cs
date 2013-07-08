d44040d800f638ff4b53aa4d4f979d040c319588
Tomas Bosak <bosak.tomas@gmail.com>
2013-07-08 16:21:05 +0200

    Add more arango specific attributes.

--- src/Arango/Arango.Client/API/Documents/ArangoProperty.cs
+++ src/Arango/Arango.Client/API/Documents/ArangoProperty.cs
@@ -5,12 +5,22 @@ namespace Arango.Client
     [AttributeUsage(AttributeTargets.Property)]
     public class ArangoProperty : Attribute
     {
-        public string Alias { get; set; }
         public bool Serializable { get; set; }
+        public bool Identity { get; set; }
+        public bool Key { get; set; }
+        public bool Revision { get; set; }
+        public bool From { get; set; }
+        public bool To { get; set; }
+        public string Alias { get; set; }
         
         public ArangoProperty()
         {
             Serializable = true;
+            Identity = false;
+            Key = false;
+            Revision = false;
+            From = false;
+            To = false;
         }
     }
 }
