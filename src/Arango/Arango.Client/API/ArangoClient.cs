4ee220d8ba7413c117c681c8eb79e2dff3e13e9d
Tomas Bosak <bosak.tomas@gmail.com>
2013-07-29 12:21:27 +0200

    Bump version.

--- src/Arango/Arango.Client/API/ArangoClient.cs
+++ src/Arango/Arango.Client/API/ArangoClient.cs
@@ -15,7 +15,7 @@ namespace Arango.Client
 
         public static string DriverVersion
         {
-            get { return "0.4.0"; }
+            get { return "0.4.1"; }
         }
 
         public static void AddDatabase(string hostname, int port, bool isSecured, string userName, string password, string alias)
