43c28d1ef1d998faacc520d9adb572938c2894b6
Tomas Bosak <bosak.tomas@gmail.com>
2013-08-06 11:11:15 +0200

    Remove unused code.

--- src/Arango/Arango.Client/API/ArangoDatabase.cs
+++ src/Arango/Arango.Client/API/ArangoDatabase.cs
@@ -43,41 +43,6 @@ namespace Arango.Client
         {
             _connection = ArangoClient.GetConnection(alias);
         }
-        
-        /*#region Query
-        
-        public List<Document> Query(string aql, out int count, Dictionary<string, object> bindVars = null, int batchSize = 0)
-        {
-            CursorOperation cursorOperation = new CursorOperation(_connection);
-            
-            return cursorOperation.Post(aql, true, out count, batchSize, bindVars);
-        }
-        
-        public List<Document> Query(string aql, Dictionary<string, object> bindVars = null, int batchSize = 0)
-        {
-            CursorOperation cursorOperation = new CursorOperation(_connection);
-            int count = 0;
-            
-            return cursorOperation.Post(aql, false, out count, batchSize, bindVars);
-        }
-        
-        public List<T> Query<T>(string aql, Dictionary<string, object> bindVars = null, int batchSize = 0) where T : class, new()
-        {
-            List<Document> documents = Query(aql, bindVars, batchSize);
-            List<T> genericCollection = new List<T>();
-            
-            foreach (Document document in documents)
-            {
-                T genericObject = document.To<T>();
-                document.MapAttributesTo(genericObject);
-                
-                genericCollection.Add(genericObject);
-            }
-            
-            return genericCollection;
-        }
-        
-        #endregion*/
     }
 }
 
