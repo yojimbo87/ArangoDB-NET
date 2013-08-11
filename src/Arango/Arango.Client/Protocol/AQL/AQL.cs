77867456d06ecce062b161d4ad0fe3de547c7d98
Tomas Bosak <bosak.tomas@gmail.com>
2013-08-11 21:02:21 +0200

    Make members constant.

--- src/Arango/Arango.Client/Protocol/AQL/AQL.cs
+++ src/Arango/Arango.Client/Protocol/AQL/AQL.cs
@@ -3,20 +3,23 @@ namespace Arango.Client.Protocol
 {
     internal static class AQL
     {
+        internal const string None = "NONE";
+        
         // high level operations
-        internal static string Collect { get { return "COLLECT"; } }
-        internal static string Filter { get { return "FILTER"; } }
-        internal static string For { get { return "FOR"; } }
-        internal static string In { get { return "IN"; } }
-        internal static string Into { get { return "INTO"; } }
-        internal static string Let { get { return "LET"; } }
-        internal static string Limit { get { return "LIMIT"; } }
-        internal static string Return { get { return "RETURN"; } }
-        internal static string Sort { get { return "SORT"; } }
+        internal const string Collect = "COLLECT";
+        internal const string Filter = "FILTER";
+        internal const string For = "FOR";
+        internal const string In = "IN";
+        internal const string Into = "INTO";
+        internal const string Let = "LET";
+        internal const string Limit = "LIMIT";
+        internal const string Return = "RETURN";
+        internal const string Sort = "SORT";
         
         // symbols
-        internal static string And { get { return "&&"; } }
-        internal new static string Equals { get { return "=="; } }
-        internal static string From { get { return ":"; } }
+        internal const string And = "&&";
+        internal const string DoubleEquals = "==";
+        internal new const string Equals = "=";
+        internal const string From = ":";
     }
 }
