ed591771bd26ab9f171541aec2c63b2bf5a5a2ca
Tomas Bosak <bosak.tomas@gmail.com>
2013-08-21 15:52:27 +0200

    Add xml docs to document operations.

--- src/Arango/Arango.Client/API/Documents/ArangoDocumentOperation.cs
+++ src/Arango/Arango.Client/API/Documents/ArangoDocumentOperation.cs
@@ -13,11 +13,17 @@ namespace Arango.Client
 
         #region Get
         
+        /// <summary>
+        /// Retrieves document object from database identified by ID.
+        /// </summary>
         public ArangoDocument Get(string id)
         {
             return _documentOperation.Get(id);
         }
         
+        /// <summary>
+        /// Retrieves document from database identified by ID and maps it to specified generic object.
+        /// </summary>
         public T Get<T>(string id) where T : class, new()
         {
             var arangoDocument = Get(id);
@@ -31,11 +37,17 @@ namespace Arango.Client
         
         #region Create
         
+        /// <summary>
+        /// Creates document in database collection and assigns additional data to referenced object.
+        /// </summary>
         public void Create(string collection, ArangoDocument arangoDocument, bool waitForSync = false, bool createCollection = false)
         {
             _documentOperation.Post(collection, arangoDocument, waitForSync, createCollection);
         }
         
+        /// <summary>
+        /// Creates document in database collection and assigns additional data to referenced object.
+        /// </summary>
         public void Create<T>(string collection, T genericObject, bool waitForSync = false, bool createCollection = false)
         {
             var arangoDocument = new ArangoDocument();
@@ -48,6 +60,9 @@ namespace Arango.Client
         
         #endregion
         
+        /// <summary>
+        /// Deletes specified document from database collection and returnes boolean value which indicates if the operation was successful.
+        /// </summary>
         public bool Delete(string id)
         {
             return _documentOperation.Delete(id);
@@ -55,11 +70,17 @@ namespace Arango.Client
         
         #region Replace
         
+        /// <summary>
+        /// Replace existing document in database collection with another document object and retrieves boolean value indicating if the operation was successful.
+        /// </summary>
         public bool Replace(ArangoDocument arangoDocument, bool waitForSync = false, string revision = null)
         {
             return _documentOperation.Put(arangoDocument, waitForSync, revision);
         }
         
+        /// <summary>
+        /// Replace existing document in database collection with another document object and retrieves boolean value indicating if the operation was successful.
+        /// </summary>
         public bool Replace<T>(T genericObject, bool waitForSync = false, string revision = null)
         {
             var arangoDocument = new ArangoDocument();
@@ -76,11 +97,17 @@ namespace Arango.Client
         
         #region Update
         
+        /// <summary>
+        /// Updates existing document in database collection and retrieves boolean value indicating if the operation was successful.
+        /// </summary>
         public bool Update(ArangoDocument arangoDocument, bool waitForSync = false, string revision = null)
         {
             return _documentOperation.Patch(arangoDocument, waitForSync, revision);
         }
         
+        /// <summary>
+        /// Updates existing document in database collection and retrieves boolean value indicating if the operation was successful.
+        /// </summary>
         public bool Update<T>(T genericObject, bool waitForSync = false, string revision = null)
         {
             var arangoDocument = new ArangoDocument();
@@ -95,6 +122,9 @@ namespace Arango.Client
         
         #endregion
         
+        /// <summary>
+        /// Check for existence of specific document.
+        /// </summary>
         public bool Exists(string id)
         {
             return _documentOperation.Head(id);
