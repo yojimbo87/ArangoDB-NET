e83f08eb7824083532530b83bb22b82985460f2b
Tomas Bosak <bosak.tomas@gmail.com>
2013-08-21 15:52:11 +0200

    Add xml docs to collection operations.

--- src/Arango/Arango.Client/API/Collections/ArangoCollectionOperation.cs
+++ src/Arango/Arango.Client/API/Collections/ArangoCollectionOperation.cs
@@ -11,21 +11,33 @@ namespace Arango.Client
             _collectionOperation = collectionOperation;
         }
 
+        /// <summary>
+        /// Retrieves collection object from database identified by its name.
+        /// </summary>
         public ArangoCollection Get(string name)
         {
             return _collectionOperation.Get(name);
         }
         
+        /// <summary>
+        /// Creates collection in database and assigns additional data to referenced object.
+        /// </summary>
         public void Create(ArangoCollection collection)
         {
             _collectionOperation.Post(collection);
         }
         
+        /// <summary>
+        /// Deletes specified collection from database and returnes boolean value which indicates if the operation was successful.
+        /// </summary>
         public bool Delete(string name)
         {
             return _collectionOperation.Delete(name);
         }
         
+        /// <summary>
+        /// Removes all documnets from specified collection and returns boolean values which indicates if the operation was successful.
+        /// </summary>
         public bool Clear(string name)
         {
             return _collectionOperation.PutTruncate(name);
