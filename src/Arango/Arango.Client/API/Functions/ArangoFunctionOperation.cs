6e563bc1ed2038d393fc544f1352393554fbdb0b
Tomas Bosak <bosak.tomas@gmail.com>
2013-11-02 10:52:55 +0100

    Add xml docs to ArangoFunctionOperation class.

--- src/Arango/Arango.Client/API/Functions/ArangoFunctionOperation.cs
+++ src/Arango/Arango.Client/API/Functions/ArangoFunctionOperation.cs
@@ -3,6 +3,9 @@ using Arango.Client.Protocol;
 
 namespace Arango.Client
 {
+    /// <summary> 
+    /// Expose AQL function management functionality.
+    /// </summary>
     public class ArangoFunctionOperation
     {
         private FunctionOperation _functionOperation;
@@ -14,11 +17,18 @@ namespace Arango.Client
         
         #region Get
         
+        /// <summary> 
+        /// Retrieves list of all registered AQL user functions.
+        /// </summary>
         public List<Document> Get()
         {
             return Get(null);
         }
         
+        /// <summary> 
+        /// Retrieves list of registered AQL user functions from specified namespace.
+        /// </summary>
+        /// <param name="namespace">Namespace from which will be AQL user functions retrieved.</param>
         public List<Document> Get(string @namespace)
         {
             return _functionOperation.Get(@namespace);
@@ -28,6 +38,11 @@ namespace Arango.Client
         
         #region Create
         
+        /// <summary> 
+        /// Creates AQL user function with specified name and code.
+        /// </summary>
+        /// <param name="name">Name of the AQL user function.</param>
+        /// <param name="code">String representation of function body.</param>
         public bool Create(string name, string code)
         {
             return _functionOperation.Post(name, code);
@@ -37,6 +52,11 @@ namespace Arango.Client
         
         #region Replace
         
+        /// <summary> 
+        /// Replaces existing AQL user function with new one.
+        /// </summary>
+        /// <param name="name">Name of the AQL user function.</param>
+        /// <param name="code">String representation of function body.</param>
         public bool Replace(string name, string code)
         {
             return _functionOperation.Post(name, code);
@@ -46,11 +66,20 @@ namespace Arango.Client
         
         #region Delete
         
+        /// <summary> 
+        /// Deletes specified AQL user function.
+        /// </summary>
+        /// <param name="name">Name of the AQL user function to be deleted.</param>
         public bool Delete(string name)
         {
             return _functionOperation.Delete(name, null);
         }
         
+        /// <summary> 
+        /// Deletes specified AQL user function.
+        /// </summary>
+        /// <param name="name">Name of the AQL user function to be deleted.</param>
+        /// <param name="group">If set to true, then the function name provided in name is treated as a namespace prefix, and all functions in the specified namespace will be deleted. If set to false, the function name provided in name must be fully qualified, including any namespaces.</param>
         public bool Delete(string name, bool group)
         {
             return _functionOperation.Delete(name, group ? "true" : "false");
