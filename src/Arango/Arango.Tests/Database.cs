67c26bef017340bf201e01f6a0075b1fd0d2aa4e
Tomas Bosak <bosak.tomas@gmail.com>
2014-12-01 17:21:43 +0100

    Create tests project with unit tests for database operations.

---
+++ src/Arango/Arango.Tests/Database.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections.Generic;
+using Arango.Client;
+
+namespace Arango.Tests
+{
+    public static class Database
+    {
+        public static string TestDatabaseOneTime { get; set; }
+        public static string TestDatabaseGeneral { get; set; }
+
+        public static string TestDocumentCollectionName { get; set; }
+        public static string TestEdgeCollectionName { get; set; }
+
+        public static string Alias { get; set; }
+        public static string SystemAlias { get; set; }
+
+        public static string Hostname { get; set; }
+        public static int Port { get; set; }
+        public static bool IsSecured { get; set; }
+        public static string UserName { get; set; }
+        public static string Password { get; set; }
+        
+        static Database()
+        {
+            TestDatabaseOneTime = "testOneTimeDatabase001xyzLatif";
+            TestDatabaseGeneral = "testDatabaseGeneral001xyzLatif";
+
+            TestDocumentCollectionName = "testDocumentCollection001xyzLatif";
+            TestEdgeCollectionName = "testEdgeCollection001xyzLatif";
+            
+            Alias = "testAlias";
+            SystemAlias = "systemAlias";
+            Hostname = "localhost";
+            Port = 8529;
+            IsSecured = false;
+            UserName = "";
+            Password = "";
+
+            ArangoSettings.AddConnection(
+                SystemAlias,
+                Hostname,
+                Port,
+                IsSecured,
+                "_system",
+                UserName,
+                Password
+            );
+
+            ArangoSettings.AddConnection(
+                Alias,
+                Hostname,
+                Port,
+                IsSecured,
+                TestDatabaseGeneral,
+                UserName,
+                Password
+            );
+        }
+        
+        public static void CreateTestDatabase(string databaseName)
+        {	
+            DeleteTestDatabase(databaseName);
+
+            var db = new ArangoDatabase(Database.SystemAlias);
+            
+            var resultList = db.GetAccessibleDatabases();
+
+            if (resultList.Success && resultList.Value.Contains(databaseName))
+            {
+            	db.Drop(databaseName);
+            }
+            
+            db.Create(databaseName);
+        }
+
+        public static void DeleteTestDatabase(string databaseName)
+        {
+            var db = new ArangoDatabase(Database.SystemAlias);
+            
+            var resultList = db.GetAccessibleDatabases();
+
+            if (resultList.Success && resultList.Value.Contains(databaseName))
+            {
+            	db.Drop(databaseName);
+            }
+        }
+
+        public static void CleanupTestDatabases()
+        {
+            Database.DeleteTestDatabase(Database.TestDatabaseGeneral);
+            Database.DeleteTestDatabase(Database.TestDatabaseOneTime);
+        }
+    }
+}
---
+++ src/Arango/Arango.Tests/DatabaseOperations/DatabaseOperationsTests.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+using Arango.Client;
+
+namespace Arango.Tests
+{
+    [TestFixture()]
+    public class DatabaseOperationsTests : IDisposable
+    {
+        [Test()]
+        public void Should_get_list_of_accessible_databases()
+        {
+            Database.CleanupTestDatabases();
+        	
+            var db = new ArangoDatabase(Database.SystemAlias);
+
+            var resultCreate = db.Create(Database.TestDatabaseOneTime);
+
+            var resultList = db.GetAccessibleDatabases();
+
+            Assert.AreEqual(200, resultList.StatusCode);
+            Assert.AreEqual(true, resultList.Success);
+            Assert.AreEqual(true, resultList.Value.Contains(Database.TestDatabaseOneTime));
+        }
+        
+        [Test()]
+        public void Should_get_list_of_all_databases()
+        {
+            Database.CleanupTestDatabases();
+        	
+            var db = new ArangoDatabase(Database.SystemAlias);
+
+            var resultCreate = db.Create(Database.TestDatabaseOneTime);
+
+            var resultList = db.GetAllDatabases();
+
+            Assert.AreEqual(200, resultList.StatusCode);
+            Assert.AreEqual(true, resultList.Success);
+            Assert.AreEqual(true, resultList.Value.Contains(Database.TestDatabaseOneTime));
+            Assert.AreEqual(true, resultList.Value.Contains("_system"));
+        }
+        
+        [Test()]
+        public void Should_get_current_database()
+        {
+            Database.CleanupTestDatabases();
+
+            var db = new ArangoDatabase(Database.SystemAlias);
+
+            var resultCurrent = db.GetCurrent();
+
+            Assert.AreEqual(200, resultCurrent.StatusCode);
+            Assert.AreEqual(true, resultCurrent.Success);
+            Assert.AreEqual("_system", resultCurrent.Value.String("name"));
+            Assert.AreEqual(false, string.IsNullOrEmpty(resultCurrent.Value.String("id")));
+            Assert.AreEqual(false, string.IsNullOrEmpty(resultCurrent.Value.String("path")));
+            Assert.AreEqual(true, resultCurrent.Value.Bool("isSystem"));
+        }
+        
+        [Test()]
+        public void Should_create_database()
+        {
+            Database.CleanupTestDatabases();
+        	
+            var db = new ArangoDatabase(Database.SystemAlias);
+
+            var resultCreate = db.Create(Database.TestDatabaseOneTime);
+
+            Assert.AreEqual(201, resultCreate.StatusCode);
+            Assert.AreEqual(true, resultCreate.Success);
+            Assert.AreEqual(true, resultCreate.Value);
+        }
+        
+        [Test()]
+        public void Should_create_database_with_users()
+        {
+            Database.CleanupTestDatabases();
+        	
+            var db = new ArangoDatabase(Database.SystemAlias);
+
+            var users = new List<DatabaseUser>()
+            {
+                new DatabaseUser { Username = "admin", Password = "secret", Active = true },
+                new DatabaseUser { Username = "tester001", Password = "test001", Active = false } 
+            };
+            
+            var resultCreate = db.Create(Database.TestDatabaseOneTime, users);
+
+            Assert.AreEqual(201, resultCreate.StatusCode);
+            Assert.AreEqual(true, resultCreate.Success);
+            Assert.AreEqual(true, resultCreate.Value);
+        }
+        
+        [Test()]
+        public void Should_fail_create_already_existing_database()
+        {
+            Database.CleanupTestDatabases();
+        	
+            var db = new ArangoDatabase(Database.SystemAlias);
+
+            var resultCreate = db.Create(Database.TestDatabaseGeneral);
+            
+            var resultCreate2 = db.Create(Database.TestDatabaseGeneral);
+
+            Assert.AreEqual(409, resultCreate2.StatusCode);
+            Assert.AreEqual(false, resultCreate2.Success);
+            Assert.IsNotNull(resultCreate2.Error);
+        }
+        
+        [Test()]
+        public void Should_fail_create_database_from_non_system_database()
+        {
+            Database.CleanupTestDatabases();
+        	
+            var db = new ArangoDatabase(Database.SystemAlias);
+
+            var resultCreate = db.Create(Database.TestDatabaseGeneral);
+
+            var nonSystemDatabase = new ArangoDatabase(Database.Alias);
+            
+            var resultCreate2 = nonSystemDatabase.Create(Database.TestDatabaseOneTime);
+
+            Assert.AreEqual(403, resultCreate2.StatusCode);
+            Assert.AreEqual(false, resultCreate2.Success);
+            Assert.IsNotNull(resultCreate2.Error);
+        }
+        
+        [Test()]
+        public void Should_delete_database()
+        {
+            Database.CleanupTestDatabases();
+        	
+            var db = new ArangoDatabase(Database.SystemAlias);
+
+            var resultCreate = db.Create(Database.TestDatabaseGeneral);
+            
+            var resultDelete = db.Drop(Database.TestDatabaseGeneral);
+
+            Assert.AreEqual(200, resultDelete.StatusCode);
+            Assert.AreEqual(true, resultDelete.Success);
+            Assert.AreEqual(true, resultDelete.Value);
+        }
+        
+        public void Dispose()
+        {
+            Database.CleanupTestDatabases();
+        }
+    }
+}
