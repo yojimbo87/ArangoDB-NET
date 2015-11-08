d27ebe672a16a401c5342294df5eaf44d17631d8
Tomas Bosak <bosak.tomas@gmail.com>
2015-11-08 16:42:21 +0100

    Add insert console test

---
+++ src/Arango/Arango.ConsoleTests/Entities/Release.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Arango.ConsoleTests
+{
+    public class Release
+    {
+        public int Id { get; set; }
+        public string Status { get; set; }
+        public string Title { get; set; }
+        public string Released { get; set; }
+        public string Country { get; set; }
+    }
+}
--- src/Arango/Arango.ConsoleTests/Program.cs
+++ src/Arango/Arango.ConsoleTests/Program.cs
@@ -9,10 +9,39 @@ namespace Arango.ConsoleTests
     {
         public static void Main(string[] args)
         {
+            InsertTest();
             //PerformanceTests();
             
             Console.Write("Press any key to continue . . . ");
             Console.ReadKey(true);
+        }
+        
+        static void InsertTest()
+        {
+            Database.CleanupTestDatabases();
+            Database.CreateTestDatabase(Database.TestDatabaseGeneral);
+            Database.CreateTestCollection("Release", ACollectionType.Document);
+            
+            var db = new ADatabase(Database.Alias);
+            
+            var album = new Release { Id = 123 , Status = "some status", Title = "some title", Country = "some country", Released = "released" };
+            var releaseKey = CreateDocument(db, "Release", album);
+        }
+        
+        static string CreateDocument(ADatabase db, string collection, object dataType)
+        {
+            var createDocumentResult = db.Document
+                .WaitForSync(false)
+                .Create(collection, dataType);
+            
+            var key = "";
+            
+            if (createDocumentResult.Success)
+            {
+                key = createDocumentResult.Value.String("_key");
+            }
+            
+            return key;
         }
         
         static void PerformanceTests()
