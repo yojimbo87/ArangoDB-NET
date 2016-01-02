92b79f65214f95c633455d1887b5580b7b4c45c5
Tomas Bosak <bosak.tomas@gmail.com>
2016-01-02 13:45:21 +0100

    Remove unnecessary code

--- src/Arango/Arango.ConsoleTests/Program.cs
+++ src/Arango/Arango.ConsoleTests/Program.cs
@@ -5,11 +5,6 @@ using Arango.fastJSON;
 
 namespace Arango.ConsoleTests
 {
-    public class IssueNo16Entity
-    {
-        public SortedList<int, bool> SortedList { get; set; }
-    }
-    
     class Program
     {
         public static void Main(string[] args)
@@ -29,23 +24,6 @@ namespace Arango.ConsoleTests
                 JsonToObjectAsDictionaryTest();
                 JsonParseTest();
             }*/
-            
-            var entity = new IssueNo16Entity();
-            entity.SortedList = new SortedList<int, bool>();
-            entity.SortedList.Add(1, true);
-            entity.SortedList.Add(2, false);
-            entity.SortedList.Add(3, false);
-            entity.SortedList.Add(4, false);
-            
-            var doc = Dictator.ToDocument(entity);
-            var json = JSON.ToNiceJSON(doc, new JSONParameters {});
-            
-            Console.WriteLine(json);
-            
-            var t = entity.SortedList.GetType();
-            
-            var des1 = JSON.ToObject<Dictionary<string, object>>(json);
-            var des2 = JSON.ToObject<IssueNo16Entity>(json);
             
             Console.Write("Press any key to continue . . . ");
             Console.ReadKey(true);
