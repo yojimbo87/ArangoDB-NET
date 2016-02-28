926b01d7f69383759cacb038b67b1876f82a87fa
Tomas Bosak <bosak.tomas@gmail.com>
2016-02-28 21:00:06 +0100

    Bump driver version

--- src/Arango/Arango.Client/Public/ASettings.cs
+++ src/Arango/Arango.Client/Public/ASettings.cs
@@ -19,7 +19,7 @@ namespace Arango.Client
         /// <summary>
         /// Determines driver version.
         /// </summary>
-        public const string DriverVersion = "0.9.3";
+        public const string DriverVersion = "0.9.4";
         
         /// <summary>
         /// Determines JSON serialization options.
