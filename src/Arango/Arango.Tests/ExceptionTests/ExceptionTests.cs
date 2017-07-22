7b801fa44777f34da347ad4d672dda6fbf036266
Tomas Bosak <bosak.tomas@gmail.com>
2017-07-22 20:32:34 +0200

    Add unit test for AException

---
+++ src/Arango/Arango.Tests/ExceptionTests/ExceptionTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using NUnit.Framework;
+using Arango.Client;
+
+namespace Arango.Tests.ExceptionTests
+{
+    [TestFixture()]
+    public class ExceptionTests : IDisposable
+    {
+        [Test()]
+        public void Should_throw_exception()
+        {
+            // given
+            ASettings.ThrowExceptions = true;
+
+            // when
+            var arangoException = Assert.Throws<AException>(() => {
+                var db = new ADatabase(Database.SystemAlias);
+                var resultCreate = db.Create("*/-+");
+            });
+
+            // then
+            Assert.IsNotNull(arangoException);
+            Assert.AreEqual(400, arangoException.StatusCode);
+        }
+
+        public void Dispose()
+        {
+            ASettings.ThrowExceptions = false;
+        }
+    }
+}
